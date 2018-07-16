cab5a71b8dd40e6d9e86f8576e2eac100ad302f8
Mattia Pietropaolo <mattia.pietropaolo@connexo.it>
2018-07-16 12:00:08 +0200

    fix recipe view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,10 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'test description', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=700&q=85'
+    new Recipe('A test recipe', 'test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
   )
+  new Recipe('A test recipe', 'test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
+)
   ];
 
   constructor() { }
