558ae59fcf7e20c26f97469314dae0522bc97197
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-07-17 17:24:24 +0200

    start impl recipe directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,17 +7,24 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
-  ),
-  new Recipe('A test recipe', 'test description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
-)
+    new Recipe('A test recipe', 'test description',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
+    ),
+    new Recipe('A test recipe', 'test description',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFXfwUNVbrA9Z3NY4NXPxhIo9Ggxipbz-4Clt08_U04YvoKBI'
+    )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
